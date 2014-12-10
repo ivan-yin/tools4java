68dd7e97ccbeeadf52456f6edde72360a7d25370
ivan-yin <020lilin@gmail.com>
2014-12-10 11:56:16 +0800

    http client

---
+++ src/main/java/com/ifzer/utils/HttpClientUtils.java
@@ -0,0 +1,21 @@
+package com.ifzer.utils;
+
+/**
+ *
+ * @author ybs
+ * @Email 020lilin@gmail.com
+ * @Date Dec 10, 2014
+ */
+public class HttpClientUtils {
+
+
+	public static void get(String url){
+
+	}
+
+
+	public static void post(String url){
+
+	}
+
+}
